6c18ca752e8c7e118ebefbe3f5d196df65bc7fbc
Hamdiibrahim <yusufhamdi137@gmail.com>
2024-12-18 11:12:34 +0300

    created  users table

--- db/models.py
+++ db/models.py
@@ -3,3 +3,11 @@ from sqlalchemy.orm import relationship, declarative_base
 
 # Base class for our models
 Base = declarative_base()
+
+# User Table
+class User(Base):
+    __tablename__ = 'users'  # Corrected here
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False, unique=True)
+    reflections = relationship('Reflection', back_populates='user')
+    activities = relationship('Activity', back_populates='user')
